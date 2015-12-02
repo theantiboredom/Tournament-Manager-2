0e5345fe992301d8b4d8f1103a1506ea53fc5a91
Ishin Iwasaki <iiwasaki@ufl.edu>
2015-12-02 11:33:50 -0500

    Assign station view

---
+++ Tournament Manager 2/AssignStationViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AssignStationViewController.swift
+//  Tournament Manager 2
+//
+//  Created by Ishin Iwasaki on 12/2/15.
+//  Copyright © 2015 Ishin Iwasaki. All rights reserved.
+//
+
+import UIKit
+
+class AssignStationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
