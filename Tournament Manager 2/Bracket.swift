a29c0dbcea8c9a423b188fdedf04dc07bc884528
Ishin Iwasaki <iiwasaki@ufl.edu>
2015-12-02 11:30:43 -0500

    Bracket

--- Tournament Manager 2/Bracket.swift
+++ Tournament Manager 2/Bracket.swift
@@ -54,7 +54,7 @@ class Bracket: NSManagedObject {
         }
             //double elimination
         else {
-            for index in 0...94
+            for index in 0...126
             {
                 let entity = NSEntityDescription.entityForName("Match", inManagedObjectContext: managedContext)
                 let createdMatch = Match(entity: entity!, insertIntoManagedObjectContext: managedContext)
@@ -72,8 +72,63 @@ class Bracket: NSManagedObject {
                     createdMatch.next_winner = div + 32
                     
                 }
+                else if (index >= 63 && index <= 78){
+                    createdMatch.next_winner = index + 16
+                }
+                else if (index >= 79 && index <= 94){
+                    if(index%2 == 0){
+                        let temp = (Double(index)-0.5)/2
+                        createdMatch.next_winner = Int(temp) + 56
+                    }
+                    else{
+                        createdMatch.next_winner = (index/2) + 56
+                    }
+                    
+                }
+                else if (index >= 95 && index <= 102){
+                    createdMatch.next_winner = index + 8
+                }
+                else if (index >= 103 && index <= 110){
+                    if(index%2 == 0){
+                        let temp = (Double(index)-0.5)/2
+                        createdMatch.next_winner = Int(temp) + 60
+                    }
+                    else{
+                        createdMatch.next_winner = (index/2) + 60
+                    }
+                }
+                else if (index >= 111 && index <= 114){
+                    createdMatch.next_winner = index + 4
+                }
+                else if (index >= 115 && index <= 118){
+                    if(index%2 == 0){
+                        let temp = (Double(index)-0.5)/2
+                        createdMatch.next_winner = Int(temp) + 62
+                    }
+                    else{
+                        createdMatch.next_winner = (index/2) + 62
+                    }
+                }
+                else if (index >= 119 && index <= 120) {
+                    createdMatch.next_winner = index + 2
+                }
+                else if (index >= 121 && index <= 122){
+                    createdMatch.next_winner = 123
+                }
+                else {
+                    //123 and above 
+                    createdMatch.next_winner = index + 1
+                }
                 
-                if index == 94 {
+                
+                //program in the next loser bracket number
+                if (index >= 0 && index <= 31){
+                    let div = index/2
+                    createdMatch.next_loser = div + 63
+                }
+                
+                
+                if index == 126 {
                     createdMatch.lastMatch = true
                 }
                 else{
